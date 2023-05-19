bee065455b2729ca01070c57e6aebf2157244062
CauaDezidera <127250310+CauaDezidera@users.noreply.github.com>
2023-05-18 21:17:35 -0300

    Add files via upload

---
+++ flask_mysqldb.sql
@@ -0,0 +1,18 @@
+create database desafio3;
+#drop database desafio3;
+
+show databases;
+
+use desafio3;
+
+create table contatos
+(
+	email varchar(60),
+    assunto varchar(60),
+    descricao varchar(200)
+);
+
+#truncate contatos;
+select * from contatos;
+
+
